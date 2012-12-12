94efa4562c0f860da71eff850344411e1030e1f9
Paul Fleischer <pfleischer@laptop-51.(none)>
2012-12-12 09:40:03 +0100

    Print color of selected project. Parse from file.

--- parser_test.c
+++ parser_test.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 const char *simple_test_string =
 " 	{ \n\
@@ -42,13 +43,20 @@ int
 main(int argc, char *argv[]) {
 	parser_init(&parserState, stateStack, 50, ST_OBJECT);
 
-	printf("%s\n", test_string);
+	FILE *file = fopen(argv[1], "r");
+
+	//printf("%s\n", test_string);
 	//printf("%s\n", array_test_string);
 
 	// We feed the parser one character at a time in order to ensure that
 	// it is stable in all cases
-	for(int i=0; i<strlen(test_string); i++) {
-		json_parse_buf(&parserState, test_string + i, 1);
+	//for(int i=0; i<strlen(test_string); i++) {
+	while(!feof(file)) {
+		char buf[1];
+		if( fread(buf, 1, 1, file) < 1 ) {
+			break;
+		}
+		json_parse_buf(&parserState, buf, 1);
 
 		if( parser_current_state(&parserState) == ST_INVALID ) {
 			printf("INVALID STATE\n");
@@ -63,30 +71,71 @@ static int level = -1;
 void
 event_handler(int event, void *data)
 {
+	static int jobsLevel = 0;
+	static char currentColor[10];
+	static bool setColor = false;
+	static bool setName = false;
+	static bool isMatch = false;
+
 	switch(event) {
 		case EVENT_STRUCT_START:
 			level++;
-			printf("START struct\n");
+			//printf("START struct\n");
+			if( jobsLevel >= 1 ) {
+				jobsLevel++;
+				isMatch = false;
+			}
 		break;
 		case EVENT_STRUCT_END:
 			level--;
-			printf("END struct\n");
+			if( jobsLevel == 3 ) {
+				if( isMatch ) 
+					printf("Color is %s\n", currentColor);
+			}
+			if( jobsLevel > 0 )
+				jobsLevel--;
+			//printf("END struct\n");
 		break;
 		case EVENT_ARRAY_START:
-			printf("START array\n");
+			//printf("START array\n");
+			if( jobsLevel == 1 ) {
+				jobsLevel++;
+			}
 		break;
 		case EVENT_ARRAY_END:
-			printf("END array\n");
+			//printf("END array\n");
+			if( jobsLevel == 2 ) {
+				jobsLevel--;
+			}
 		break;
 		case EVENT_KEY:
 			strcpy(names[level], (char*)data);
 			//printf("Key: %s\n", (char*)data);
+			if( level == 0 && strncmp("jobs", data, 4) == 0) {
+				jobsLevel++;
+			}
+			if( jobsLevel == 3 && strncmp((char*)data, "color", 5) == 0) {
+				setColor = true;
+			} else {
+				setColor = false;
+			}
+
+			if( jobsLevel == 3 && strncmp((char*)data, "name", 4) == 0) {
+				setName = true;
+			} else {
+				setName = false;
+			}
 		break;
 		case EVENT_STRING:
 			for(int i=0; i<=level; i++) {
-				printf("%s.", names[i]);
+				//printf("%s.", names[i]);
 			}
-			printf(" = %s\n", (char*)data);
+			//printf(" = %s\n", (char*)data);
+			if( setColor && jobsLevel == 3 ) {
+				strcpy(currentColor, (char*)data);
+			} else if( setName && jobsLevel == 3 && strncmp((char*)data, "api-client-base", 15) == 0) {
+				isMatch = true;
+			}
 		break;
 		default:
 		break;
